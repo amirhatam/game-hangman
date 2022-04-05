3e480c7680dc035a6d15cdfc5558f559efbdb45d
Amir <amirhatam136@gmail.com>
2022-04-05 17:19:51 +0200

    remove node M 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MDBContainer } from 'mdb-react-ui-kit';
 import { Pendu } from './views/Pendu';
 import Navbar from './componentes/Navbar';
 import { Footer } from './componentes/Footer';
