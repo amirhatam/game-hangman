0baa8b33068c2ab72655a2d3b17a39bf45152f63
Amir <amirhatam136@gmail.com>
2022-04-05 17:08:15 +0200

    first commit

--- src/componentes/Dictionary.jsx
+++ src/componentes/Dictionary.jsx
@@ -26,7 +26,6 @@ export const Dictionary = (
                     const verbData = response.data[0].meanings[1]
                     // console.log(response.data[0].meanings[0].partOfSpeech);
                     // console.log(response.data[0].meanings[1].partOfSpeech);
-                    // PartOfSpeech Noun
                     nounData.synonyms.map(e => nounSyns.push(e))
                     nounData.antonyms.map(e => nounAnts.push(e))
 
